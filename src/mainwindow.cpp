a63d16257b6688cd59734d70603efd4580be999b
Cam K <none>
2021-10-19 01:26:30 -0400

    Add project files

---
+++ src/mainwindow.cpp
@@ -0,0 +1,9 @@
+#include <QMainWindow>
+
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{ }
